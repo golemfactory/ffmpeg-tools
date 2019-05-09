07c494fe13491fe0764115e8245107ea2cde4d47
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-05-09 16:51:58 +0200

    Updated url in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setuptools.setup(
     name='ffmpeg-tools',
     version='0.1.0',
     description="Tools for using ffmpeg functionalities in python.",
-    #url='https://github.com/golemfactory/golem-verificator',
+    url='https://github.com/golemfactory/ffmpeg-tools',
     maintainer='The Golem Team',
     maintainer_email='tech@golem.network',
-    packages=setuptools.find_packages(),
-    python_requires='>=3.5'
+    packages=setuptools.find_packages(exclude=["tests/"]),
+    python_requires='>=3.5',
+    zip_safe=False
 )
 
