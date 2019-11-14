164fdacd19ee2f4e9ef2b794a626e90c64e80df5
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-11-15 00:54:52 +0100

    FrameRate: A cleaner implementation of normalization and conversions as methods on the class

--- ffmpeg_tools/formats.py
+++ ffmpeg_tools/formats.py
@@ -1,6 +1,6 @@
 import enum
 from math import gcd
-from typing import NamedTuple
+from typing import Any, NamedTuple, Union
 
 from . import validation
 from . import codecs
@@ -348,6 +348,68 @@ _aspect_ratio_overrides = {
 class FrameRate(NamedTuple):
     dividend: int
     divisor: int = 1
+
+    @classmethod
+    def decode(cls, raw_value: Any) -> 'FrameRate':
+        if isinstance(raw_value, str):
+            return cls.from_string(raw_value)
+        if isinstance(raw_value, int):
+            return cls.from_collection((raw_value,))
+        if isinstance(raw_value, float):
+            int_value = int(raw_value)
+            if raw_value == int_value:
+                return cls.from_collection((int_value,))
+
+            raise ValueError(
+                "Expressing frame rate using a floating point number is not supported. "
+                "To get accurate results you should always specify it as a ratio of two integers."
+            )
+        if isinstance(raw_value, FrameRate):
+            return raw_value
+        if isinstance(raw_value, (list, tuple)):
+            return cls.from_collection(raw_value)
+
+        raise ValueError(f"Value of type {type(raw_value)} could not be interpreted as frame rate")
+
+    @classmethod
+    def from_collection(cls, collection: Union[list, tuple]) -> 'FrameRate':
+        if len(collection) not in [1, 2] or not all(isinstance(i, int) for i in collection):
+            raise ValueError(f"Only (int, int), or (int,) can be interpreted as a frame rate")
+
+        if len(collection) == 1:
+            (dividend, divisor) = (collection[0], 1)
+        else:
+            (dividend, divisor) = (collection[0], collection[1])
+
+        if dividend < 0 or divisor < 0:
+            raise ValueError("Frame rate can't be negative")
+
+        if divisor == 0:
+            raise ValueError("Divisor of the frame rate can't be zero")
+
+        return cls(dividend, divisor)
+
+    @classmethod
+    def from_string(cls, string_value: str) -> 'FrameRate':
+        return cls.from_collection([int(i) for i in string_value.split('/', 1)])
+
+    def normalized(self) -> 'FrameRate':
+        assert self.dividend >= 0
+        assert self.divisor > 0
+
+        common_divisor = gcd(self.dividend, self.divisor)
+        return FrameRate(
+            self.dividend // common_divisor,
+            self.divisor // common_divisor,
+        )
+
+    def to_float(self) -> float:
+        return self.dividend / self.divisor
+
+    def __str__(self):
+        # ASSUMPTION: this representation is unambiguous and directly usable
+        # as a command-line argument for ffmpeg
+        return f"{self.dividend}/{self.divisor}"
 
 
 _frame_rates = {
--- tests/test_formats.py
+++ tests/test_formats.py
@@ -188,3 +188,124 @@ class TestFrameRate(TestCase):
         self.assertEqual(ffmpeg.formats.FrameRate(10), (10, 1))
         self.assertEqual(ffmpeg.formats.FrameRate(10, 5), (10, 5))
         self.assertEqual(ffmpeg.formats.FrameRate(10, 6), (10, 6))
+
+    @parameterized.expand([
+        (ffmpeg.formats.FrameRate(30), (30, 1)),
+        (ffmpeg.formats.FrameRate('30'), ('30', 1)),
+        (0, (0, 1)),
+        (30, (30, 1)),
+        (30.0, (30, 1)),
+        ('30', (30, 1)),
+        ('30/1', (30, 1)),
+        ((30, 1), (30, 1)),
+        ([30, 1], (30, 1)),
+    ])
+    def test_decode_should_parse_valid_representations(self, raw_value, expected_tuple):
+        decoded_frame_rate = ffmpeg.formats.FrameRate.decode(raw_value)
+        self.assertIsInstance(decoded_frame_rate, ffmpeg.formats.FrameRate)
+        self.assertEqual(decoded_frame_rate, ffmpeg.formats.FrameRate(*expected_tuple))
+
+    @parameterized.expand([
+        ('',),
+        ('abc',),
+        ('30.0'),
+        ('30.1'),
+        ('30/30/30',),
+        (-30,),
+        (30.1,),
+        ((),),
+        ([],),
+        ((30, 1, 30),),
+        ((30.1, 1),),
+        ({},),
+        (set(),),
+    ])
+    def test_decode_should_reject_invalid_representations(self, raw_value):
+        with self.assertRaises(ValueError):
+            ffmpeg.formats.FrameRate.decode(raw_value)
+
+    @parameterized.expand([
+        (ffmpeg.formats.FrameRate(30), (30, 1)),
+        ((30, 1), (30, 1)),
+        ([30, 1], (30, 1)),
+        ((0,), (0, 1)),
+        ((30,), (30, 1)),
+        ((30, 1), (30, 1)),
+        ((60, 2), (60, 2)),
+        ((62, 4), (62, 4)),
+    ])
+    def test_from_collection_should_parse_valid_representations(self, collection, expected_tuple):
+        parsed_frame_rate = ffmpeg.formats.FrameRate.from_collection(collection)
+        self.assertIsInstance(parsed_frame_rate, ffmpeg.formats.FrameRate)
+        self.assertEqual(parsed_frame_rate, ffmpeg.formats.FrameRate(*expected_tuple))
+
+    @parameterized.expand([
+        ((),),
+        ([],),
+        ((30, 1, 30),),
+        ([30, 1, 30],),
+        ((30.0, 1),),
+        ((30.1, 1),),
+        ((30, '1'),),
+        ((30, 1.5),),
+        ((-1,),),
+        ((10, -10),),
+        ((-10, 10),),
+        ((-10, -10),),
+        ((10, 0),),
+        (ffmpeg.formats.FrameRate('30'),),
+    ])
+    def test_from_collection_should_reject_invalid_representations(self, collection):
+        with self.assertRaises(ValueError):
+            ffmpeg.formats.FrameRate.from_collection(collection),
+
+    @parameterized.expand([
+        ('0', (0, 1)),
+        ('30', (30, 1)),
+        ('30/1', (30, 1)),
+        ('60/2', (60, 2)),
+        ('62/4', (62, 4)),
+    ])
+    def test_from_string_should_parse_valid_representations(self, string_value, expected_tuple):
+        parsed_frame_rate = ffmpeg.formats.FrameRate.from_string(string_value)
+        self.assertIsInstance(parsed_frame_rate, ffmpeg.formats.FrameRate)
+        self.assertEqual(parsed_frame_rate, ffmpeg.formats.FrameRate(*expected_tuple))
+
+    @parameterized.expand([
+        ('',),
+        ('abc',),
+        ('29.5',),
+        ('30/1/2',),
+        ('30/',),
+        ('/1',),
+        ('30/1.5',),
+        ('-1',),
+        ('10/-10',),
+        ('-10/10',),
+        ('-10/-10',),
+    ])
+    def test_from_string_should_reject_invalid_representations(self, string_value):
+        with self.assertRaises(ValueError):
+            ffmpeg.formats.FrameRate.from_string(string_value),
+
+    @parameterized.expand([
+        ((0, 1), (0, 1)),
+        ((30, 1), (30, 1)),
+        ((5, 13), (5, 13)),
+        ((60, 2), (30, 1)),
+        ((62, 4), (31, 2)),
+    ])
+    def test_normalized_should_return_values_without_common_divisors(self, input_tuple, expected_tuple):
+        normalized_frame_rate = ffmpeg.formats.FrameRate(*input_tuple).normalized()
+        self.assertIsInstance(normalized_frame_rate, ffmpeg.formats.FrameRate)
+        self.assertEqual(normalized_frame_rate, ffmpeg.formats.FrameRate(*expected_tuple))
+
+    def test_to_float(self):
+        self.assertEqual(ffmpeg.formats.FrameRate(10).to_float(), 10.0)
+        self.assertEqual(ffmpeg.formats.FrameRate(5, 2).to_float(), 2.5)
+        self.assertEqual(ffmpeg.formats.FrameRate(10, 2).to_float(), 5.0)
+
+    def test_should_have_unambiguous_string_representation(self):
+        self.assertEqual(str(ffmpeg.formats.FrameRate(10)), "10/1")
+        self.assertEqual(str(ffmpeg.formats.FrameRate(10, 5)), "10/5")
+        self.assertEqual(str(ffmpeg.formats.FrameRate(10, 6)), "10/6")
