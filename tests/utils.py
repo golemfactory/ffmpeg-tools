b4860494ea6f67040cc7bc3b7dc91563b8653fc7
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-11-28 20:27:05 +0100

    tests.utils: A test helper for automatic test name generation for parameterized tests
    - We don't really want to have to write it from scratch each time and the default names do not make it easy to discern tests.

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,7 @@
 import os
+from typing import List
+
+from ffmpeg_tools import commands
 
 
 def get_absolute_resource_path(filename: str) -> str:
@@ -7,3 +10,15 @@ def get_absolute_resource_path(filename: str) -> str:
         'resources',
         filename
     )
+
+
+def make_parameterized_test_name_generator_for_scalar_values(scalar_names: List[str]):
+    def test_case_name_generator(testcase_func, param_num, param):
+        assert len(param[0]) == len(scalar_names)
+
+        scalar_values = [str(value) for value in param[0]]
+        parameter_names_and_values = '_'.join(commands.flatten_list(zip(scalar_names, scalar_values)))
+
+        return f'{testcase_func.__name__}_{param_num}_{parameter_names_and_values}'
+
+    return test_case_name_generator
