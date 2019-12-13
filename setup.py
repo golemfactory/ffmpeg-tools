d55a011ee789484f5321d9a9d62dbd102696895a
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-12-13 11:56:22 +0100

    Bump version to 0.20.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ tests_require = [
 
 setuptools.setup(
     name='ffmpeg-tools',
-    version='0.19.0',
+    version='0.20.0',
     description="Tools for using ffmpeg functionalities in python.",
     url='https://github.com/golemfactory/ffmpeg-tools',
     maintainer='The Golem Team',
